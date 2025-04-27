337edb99ad4ad6cc3b9298bb8fa5e72ebf3435c0
Muhammad <Muhammadabdelwahhab2@gmail.com>
2025-04-28 01:34:01 +0300

    first commit

---
+++ External.js
@@ -0,0 +1,19 @@
+console.log(number1);
+// It will be undefined
+var number1 = 3;
+var number2 = 2.9;
+var number3 = 0xff;
+
+console.log(number3);
+// print to know the real value 
+var fName = "Muhammad";
+var sName = 'Abdelwahhab';
+var lName = `Elzemety`;
+
+console.log("This is an External Javascript File");
+
+
+
+
+    
+ 
